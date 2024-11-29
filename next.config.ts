03ef49fb3c4ed866ffd5c8aa64780546e76c5250
MukundSinghRajput <mukundsrajput@gmail.com>
2024-11-30 01:08:33 +0530

    ✨

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs'
+      },
+      {
+        source: '/:path*',
+        destination: 'http://127.0.0.1:8000/:path*'
+      }
+    ];
+  }
 };
 
 export default nextConfig;
